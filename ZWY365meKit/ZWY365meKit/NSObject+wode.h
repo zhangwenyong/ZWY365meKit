588d5558d6992d3c38a83436b4b409ed5122d369
张文勇 <>
2018-01-11 13:23:49 +0800

    1

---
+++ ZWY365meKit/ZWY365meKit/NSObject+wode.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+wode.h
+//  ZWY365meKit
+//
+//  Created by 张文勇 on 2018/1/11.
+//  Copyright © 2018年 张文勇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (wode)
+
+@end
---
+++ ZWY365meKit/ZWY365meKit/NSObject+wode.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+wode.m
+//  ZWY365meKit
+//
+//  Created by 张文勇 on 2018/1/11.
+//  Copyright © 2018年 张文勇. All rights reserved.
+//
+
+#import "NSObject+wode.h"
+
+@implementation NSObject (wode)
+
+@end
