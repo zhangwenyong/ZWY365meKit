1f2dc61414f743a219dd5087dc7c672f14ce1036
张文勇 <>
2018-01-11 13:44:54 +0800

    23

---
+++ ZWY365meKit/myview.h
@@ -0,0 +1,12 @@
+//
+//  myview.h
+//  
+//
+//  Created by 张文勇 on 2018/1/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myview : UIView
+
+@end
---
+++ ZWY365meKit/myview.m
@@ -0,0 +1,20 @@
+//
+//  myview.m
+//  
+//
+//  Created by 张文勇 on 2018/1/11.
+//
+
+#import "myview.h"
+
+@implementation myview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
